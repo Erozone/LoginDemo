83685b6be23571a49acbbc508719a02e91a82021
Erozone <Erozone@Mohits-Mac-mini.local>
2017-01-09 15:40:06 +0530

    This is my 1st commite,Making Login Screen UI

---
+++ LoginDemo/DesignableView.swift
@@ -0,0 +1,19 @@
+//
+//  DesignableView.swift
+//  LoginDemo
+//
+//  Created by Mohit Kumar on 09/01/17.
+//  Copyright © 2017 Mohit Kumar. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableView: UIView {
+
+    @IBInspectable var roundCorner: CGFloat = 0.0{
+        didSet{
+            self.layer.cornerRadius = roundCorner
+        }
+    }
+
+}
